d306ac21599c5d9c9405c36496bc4848f106c44a
Gary Niu <gary.niu@gmail.com>
2018-08-12 21:31:28 +0800

    add jdk which download latest jdk

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,3 +1,3 @@
 export GOROOT=/usr/local/opt/go/libexec
-export GOPATH=/Users/gniu/Workbench.localized/gocodes
+export GOPATH=/Users/gniu/OneDrive/Workspaces/gocodes
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
