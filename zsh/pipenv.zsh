130a38a541a486a7b74960d632c55aea5561b9c0
Gary Niu <gary.niu@gmail.com>
2018-10-01 10:52:32 +0800

    add envs

--- zsh/pipenv.zsh
+++ zsh/pipenv.zsh
@@ -1,5 +1,5 @@
-export PATH=~/.local/bin:$PATH
-alias prp="pipenv run python"
+#export PATH=~/.local/bin:$PATH
+#alias prp="pipenv run python"
 
 #doesn't support oh-my-zsh
 #eval "$(pipenv --completion)"
